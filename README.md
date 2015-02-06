fba6c910f2bec82d6a941b819984d87072510240
wick-ed <wick.b@hotmail.de>
2015-02-06 13:17:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# PHP Metaobject Protocol
+
+[![Latest Stable Version](https://poser.pugx.org/appserver-io-psr/metaobject/v/stable.png)](https://packagist.org/packages/appserver-io-psr/metaobject) [![Total Downloads](https://poser.pugx.org/appserver-io-psr/metaobject/downloads.png)](https://packagist.org/packages/appserver-io-psr/metaobject) [![License](https://poser.pugx.org/appserver-io-psr/metaobject/license.png)](https://packagist.org/packages/appserver-io-psr/metaobject) [![Build Status](https://travis-ci.org/appserver-io-psr/metaobject.png)](https://travis-ci.org/appserver-io-psr/metaobject) [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/appserver-io-psr/metaobject/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/appserver-io-psr/metaobject/?branch=master) [![Code Coverage](https://scrutinizer-ci.com/g/appserver-io-psr/metaobject/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/appserver-io-psr/metaobject/?branch=master)
+
+## Introduction
+
+Contains the [Metaobject Protocol](https://en.wikipedia.org/wiki/Metaobject#Metaobject_protocol) classes and interfaces used to transform and enhance code. Allows for the usage of techniques such as [AOP](https://en.wikipedia.org/wiki/Aspect-oriented_programming) and [Design by Contract](https://en.wikipedia.org/wiki/Design_by_contract) within the appserver.io environment.
+
+# External Links
+
+* Documentation at [appserver.io](http://docs.appserver.io)
---
+++ src/AppserverIo/Psr/MetaobjectProtocol/Dbc/Annotations/Requires.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * \AppserverIo\Psr\MetaobjectProtocol\Dbc\Annotations\Requires
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Bernhard Wick <bw@appserver.io>
+ * @copyright 2015 TechDivision GmbH - <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io/doppelgaenger
+ * @link      http://www.appserver.io/
+ */
+
+namespace AppserverIo\Psr\MetaobjectProtocol\Dbc\Annotations;
+
+/**
+ * Annotation class which is used to identify the pre-condition a method execution or property access must fulfill
+ *
+ * @author    Bernhard Wick <bw@appserver.io>
+ * @copyright 2015 TechDivision GmbH - <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io/doppelgaenger
+ * @link      http://www.appserver.io/
+ *
+ * @Annotation
+ * @Target({"METHOD"})
+ */
+class Requires
+{
+    /**
+     * The annotation which identifies this annotation class
+     *
+     * @var string
+     */
+    const ANNOTATION = 'Requires';
+
+    /**
+     * This method returns the class name as a string
+     *
+     * @return string
+     */
+    public static function __getClass()
+    {
+        return __CLASS__;
+    }
+}
